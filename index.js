34333afb05738b51a565ed9609c6f3982983a422
gatopotato <samkitjai09@gmail.com>
2024-07-05 17:15:26 +0530

    options route

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
-    origin:*
+    origin:"*",
+    methods: "GET, POST, PUT, DELETE,OPTIONS",
+    allowedHeaders: "Content-Type",
+    credentials: true
 }));
 
 app.get('/', (req, res) => {
@@ -13,6 +16,15 @@ app.get('/', (req, res) => {
 app.post('/login', (req, res) => {
     res.json({ message: 'Login Success!' });
     } );
+
+app.options("/*", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.send();
+    });
+
 app.listen(port, () => {
     console.log('Server is running on port '+ port);
     });
