a00376c7afe12df7edf446ad8ae180e65b3f940a
Samkit Jaina <70890479+gatopotato@users.noreply.github.com>
2024-07-05 16:46:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import cors from 'cors';
 import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin:*
+}));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' });
